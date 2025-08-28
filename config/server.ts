253d7b000959d29a625049ced5413631e2905cf0
VMG-Clearl0v3 <ph.vtru0ng.1610@gmail.com>
2025-08-28 10:18:05 +0700

    sua file config, env

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
-  host: '0.0.0.0',
-  port: env.int('PORT', 1337),  // Render sẽ truyền PORT vào biến môi trường
+  host: '0.0.0.0', 
+  port: env.int('PORT', 1337), // Render sẽ override PORT
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS', ['defaultKeyA', 'defaultKeyB']),
   },
 });
